1e09cc2fac4edb1776b614bfa5981ecde8dd6c6a
João Ribeiro <joaosoft@gmail.com>
2018-04-22 16:06:02 +0100

    fix

--- service/global.go
+++ service/global.go
@@ -3,7 +3,7 @@ package godropbox
 import "github.com/joaosoft/go-log/service"
 
 var global = make(map[string]interface{})
-var log = golog.NewLogDefault("go-Dropbox", golog.InfoLevel)
+var log = golog.NewLogDefault("go-dropbox", golog.InfoLevel)
 
 func init() {
 	global[path_key] = defaultPath
