25a762506cb30d6864bdb9b65592f9534100ab3b
João Ribeiro <joao.ribeiro@foursource.pt>
2018-07-20 15:53:32 +0100

    fix

--- config.go
+++ config.go
@@ -22,7 +22,7 @@ type ElasticConfig struct {
 // NewConfig ...
 func NewConfig(endpoint string) *ElasticConfig {
 	appConfig := &AppConfig{}
-	if _, err := gomanager.NewSimpleConfig(fmt.Sprintf("/config/models.%s.json", getEnv()), appConfig); err != nil {
+	if _, err := gomanager.NewSimpleConfig(fmt.Sprintf("/config/app.%s.json", getEnv()), appConfig); err != nil {
 		log.Error(err.Error())
 	}
 
